44155d6f270b89e6765d0e3ce17bb344b03ebce8
yancarvalho13 <yansilva303@gmail.com>
2025-03-27 13:41:08 -0300

    fix: fixed queries context, now returning the exact necessity

--- src/main/resources/sql/queries/top10Trimestre.sql
+++ src/main/resources/sql/queries/top10Trimestre.sql
@@ -4,9 +4,11 @@ SELECT
     SUM(ofin.vl_saldo_final - ofin.vl_saldo_inicial) AS despesa_total
 FROM operadora_financeiro ofin
          JOIN operadoras_saude os ON ofin.reg_ans = CAST(os.registro_ans AS INT)
-WHERE TRIM(ofin.descricao) = 'Despesas com Eventos / Sinistros'
+WHERE
+    (ofin.descricao ILIKE '%sinistro%' OR ofin.descricao ILIKE '%evento%')
+  AND ofin.descricao ILIKE '%medico hospitalar%'
   AND ofin.data_registro >= date_trunc('quarter', CURRENT_DATE - INTERVAL '3 months')
   AND ofin.data_registro < date_trunc('quarter', CURRENT_DATE)
 GROUP BY os.registro_ans, os.razao_social
 ORDER BY despesa_total DESC
-    LIMIT 10;
+LIMIT 10;
