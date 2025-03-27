d19e7f6800066c96d8fe2f7a4f743c607bd4d34f
yancarvalho13 <yansilva303@gmail.com>
2025-03-27 13:29:59 -0300

    feat: added elastic search queries

--- src/main/resources/sql/queries/top10Anual.sql
+++ src/main/resources/sql/queries/top10Anual.sql
@@ -1,7 +1,12 @@
-SELECT reg_ans, descricao, SUM(vl_saldo_final - vl_saldo_inicial) AS total_despesas
-FROM operadora_financeiro
-WHERE descricao = 'EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR'
-  AND data_registro >= CURRENT_DATE - INTERVAL '1 year'
-GROUP BY reg_ans, descricao
-ORDER BY total_despesas DESC
+SELECT
+    os.registro_ans,
+    os.razao_social,
+    SUM(ofin.vl_saldo_final - ofin.vl_saldo_inicial) AS despesa_total
+FROM operadora_financeiro ofin
+         JOIN operadoras_saude os ON ofin.reg_ans = CAST(os.registro_ans AS INT)
+WHERE TRIM(ofin.descricao) = 'Despesas com Eventos / Sinistros'
+  AND ofin.data_registro >= (CURRENT_DATE - INTERVAL '1 year')
+  AND ofin.data_registro <= CURRENT_DATE
+GROUP BY os.registro_ans, os.razao_social
+ORDER BY despesa_total DESC
     LIMIT 10;
